f9ae9bd514b5929b1e5bce5a947d9a46c73ef33c
JoyAnna Walgren <joywalgren@gmail.com>
2025-04-16 22:31:22 -0600

    merge issue/1

--- Final_Project/ship.py
+++ Final_Project/ship.py
@@ -1,11 +1,20 @@
+# from cell import Cell
+
+
 class Ship:
     def __init__(self, size):
         self.size = size
         self.hits = 0
 
     def is_hit(self):
+        """Registers a hit on the ship"""
         self.hits += 1
         return self.is_sunk()
 
+    def get_size(self) -> int:
+        """Returns the size of the ship"""
+        return self.size
+
     def is_sunk(self):
+        """Checks if the ship is sunk"""
         return self.hits >= self.size
